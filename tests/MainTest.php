03e8b14d418395f077647da0da7884a64e93d8c4
Jakub Jasiński (Dzusmin) <dzusmin@gmail.com>
2022-09-11 21:49:26 +0200

    tests

---
+++ tests/MainTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use App\Main;
+use PHPUnit\Framework\TestCase;
+
+class MainTest extends TestCase
+{
+    /** @test */
+    public function it_should_test_main(): void
+    {
+        $main = new Main();
+        $this->assertSame(0, $main->main());
+    }
+}
