edef2d4f94c2435638b5a4b49d919ea2dd319b23
Jakub Jasiński (Dzusmin) <dzusmin@gmail.com>
2022-09-11 21:45:51 +0200

    test

--- src/Main.php
+++ src/Main.php
@@ -4,7 +4,16 @@ declare(strict_types=1);
 
 namespace App;
 
+use Dzusmin\POCPackage\ArrayReturningService;
+
 class Main
 {
+    public function main(): int
+    {
+        $array = [];
+        $service = new ArrayReturningService();
+        $newArray = $service->returnArray($array);
 
+        return count($array) - count($newArray);
+    }
 }
